801df7536a7ae68d804af1b4b31602ddc44e5e56
Isaac David Zolana <HiZackDavid@hotmail.com>
2023-07-13 06:56:54 -0400

    Update sql file

--- init.sql
+++ init.sql
@@ -14,7 +14,7 @@ GRANT CONNECT ON DATABASE TestStageDB TO ialab;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO ialab;
 
 INSERT INTO exemple (id, text, vector)
-VALUES ('uuid1', 'Le premier texte.', '{0.1, 0.2, 0.3, 0.4, 0.5}')
+VALUES ('uuid1', 'Le premier texte.', '{0.1, 0.2, 0.3, 0.4, 0.5}');
 
 INSERT INTO exemple (id, text, vector)
-VALUES ('uuid2', 'Le premier texte.', '{0.6, 0.7, 0.8, 0.9, 1.0}')
+VALUES ('uuid2', 'Le premier texte.', '{0.6, 0.7, 0.8, 0.9, 1.0}');
